016fbb48e67a80429da95e829febe7f29dd62b1c
Oleg <shpadio@gmail.com>
2021-02-14 03:21:38 +0300

    created app config,auth routes,jwt tokens

--- back/middlewares/index.js
+++ back/middlewares/index.js
@@ -1,9 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+// import dataBaseConnect from "./dbConnect.js";
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser'
+import expressSession from 'express-session'
+import sessionFileStore from 'session-file-store'
 
-const config = (app) => {
+const FileStore = sessionFileStore(expressSession)
+
+export const config = (app) => {
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors);
+  app.use(express.json())
+  app.use(cors());
+  app.use(bodyParser.json())
+  app.use(cookieParser())
+  // dataBaseConnect()
+  app.use(expressSession({
+    store: new FileStore(),
+    key:'auth',
+    resave:false,
+    saveUninitialized:false,
+    secret:'superdupersecret',
+    cookie:{
+      expires:6000000,
+      httpOnly:false
+    }
+  }))
 };
 
-export default config;
+
