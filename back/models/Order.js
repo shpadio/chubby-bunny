68963ea3e37ae57dc1dce2671ea504cb364ce812
Sergey E <oldskuul@mail.ru>
2021-02-15 15:08:15 +0300

    Order model rev v1

--- back/models/Order.js
+++ back/models/Order.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 const Order = mongoose.model('Order', {
+  orderNumber: Number,
   customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   ingredients: { type: mongoose.Schema.Types.ObjectId, ref: 'Ingredients' },
+  dateOfOrder: { type: Date, default: Date.now },
 });
 
 export default Order;
