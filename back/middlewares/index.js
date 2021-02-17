bc32ec49854f8183aeb446a3c5932fd874683415
Oleg <shpadio@gmail.com>
2021-02-18 01:26:34 +0300

    tried to fix creating cookies

--- back/middlewares/index.js
+++ back/middlewares/index.js
@@ -1,18 +1,33 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import sessionStore from 'session-file-store';
 
 import dotenv from 'dotenv';
 import dataBaseConnect from './dbConnect.js';
 
+const FileStore = sessionStore(session);
+
 const config = (app) => {
   dotenv.config();
-  app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
   app.use(cors());
   app.use('/public', express.static('public'));
   app.use(express.json());
 
   dataBaseConnect();
+  app.use(session({
+    store: new FileStore(),
+    name: 'auth',
+    secret: 'superman',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60000000,
+      httpOnly: false,
+    },
+  }));
 };
 export default config;
