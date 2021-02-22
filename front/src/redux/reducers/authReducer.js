9f38b8f68048c979c77862e8ce0a9e50fe9dd65d
Sergey <oldskuul@mail.ru>
2021-02-22 16:46:50 +0300

    revise reducers v1

--- front/src/redux/reducers/authReducer.js
+++ front/src/redux/reducers/authReducer.js
@@ -1,6 +1,17 @@
 import { LOGOUT, AUTH_SUCCESSFULLY, INIT_PROFILE } from '../types';
 
-const authReducer = (state = { isAuth: false, user: {} }, action) => {
+const windowState = JSON.parse(window.localStorage.getItem('state'));
+let preloadState = {};
+if (windowState && windowState.auth) {
+  preloadState = {
+    isAuth: windowState.auth.isAuth,
+    user: windowState.auth.user
+  };
+} else {
+  preloadState = { isAuth: false, user: {} };
+}
+
+const authReducer = (state = preloadState, action) => {
   switch (action.type) {
     case AUTH_SUCCESSFULLY:
       return {
@@ -9,6 +20,7 @@ const authReducer = (state = { isAuth: false, user: {} }, action) => {
     case INIT_PROFILE:
       return { ...state, user: { ...state.user, orders: action.payload } };
     case LOGOUT:
+      window.localStorage.removeItem('state');
       window.localStorage.removeItem('token');
       return {
         ...state, isAuth: false, user: { }
