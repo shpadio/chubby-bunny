678f4809b27f9bae840ab4d7b2e5b5ac483073ec
Oleg <shpadio@gmail.com>
2021-02-27 20:15:13 +0300

    fixed a bug with an existing email

--- back/routes/profile.js
+++ back/routes/profile.js
@@ -17,10 +17,14 @@ router.route('/:id')
   .put(async (req, res) => {
     const { id } = req.params;
     const { name, email } = req.body;
-    const user = await User.findByIdAndUpdate({ _id: id }, { name, email });
-    if (user) {
-      res.status(200).end();
-    } else {
+    try {
+      const user = await User.findByIdAndUpdate({ _id: id }, { name, email });
+      if (user) {
+        res.status(200).end();
+      } else {
+        throw Error('Такая почта уже зарегистрирована!');
+      }
+    } catch (err) {
       res.status(501).end();
     }
   });
