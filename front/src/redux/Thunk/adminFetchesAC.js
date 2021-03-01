3f89d06f92478c76139c5d9c8d372862d6d4f66a
Sergey <oldskuul@mail.ru>
2021-03-01 19:31:52 +0300

    hide products thunk func

--- front/src/redux/Thunk/adminFetchesAC.js
+++ front/src/redux/Thunk/adminFetchesAC.js
@@ -34,7 +34,7 @@ export const hideProductsFetchAC = (id) => (dispatch) => {
     headers: {
       'content-type': 'application/json'
     },
-    body: JSON.stringify({ id })
+    body: JSON.stringify({ id }) 
   }).then((response) => (response.json()))
     .then((data) => dispatch(hideProductsAC(data._id)));
 };
